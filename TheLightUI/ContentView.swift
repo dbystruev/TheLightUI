f815225760e86cb5357b4eeb573cca01b538c757
Denis Bystruev <dbystruev@me.com>
2020-07-24 16:06:15 +0300

    Initial Commit

---
+++ TheLightUI/ContentView.swift
@@ -0,0 +1,28 @@
+//
+//  ContentView.swift
+//  TheLightUI
+//
+//  Created by Denis Bystruev on 24.07.2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State var isLightOn = true
+    
+    var body: some View {
+        ZStack {
+            isLightOn ? Color.white : Color.black
+        }
+        .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+        .onTapGesture {
+            isLightOn.toggle()
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TheLightUI/TheLightUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheLightUIApp.swift
+//  TheLightUI
+//
+//  Created by Denis Bystruev on 24.07.2020.
+//
+
+import SwiftUI
+
+@main
+struct TheLightUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
